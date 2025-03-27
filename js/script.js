fd6db7054a0f3faaced5035bdc7fc16b4f6fa6ab
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-27 20:26:21 +0100

    [UPD] create let variables for countdown seconds

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const countdown = document.getElementById('countdown');
 const numbersList = document.getElementById('numbers-list');
 const numberForm = document.getElementById('answers-form');
 
+// dichiaro una variabile per i secondi
+let seconds = 10;
+
+// assegno i secondi all'interno del countdown
+countdown.innerText = seconds;
+
