0e5dd882ae9100d09a32c30d2a875ff7ac8a26b1
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-27 20:21:14 +0100

    [UPD] get elements from html

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// prendo gli elementi dalla pagina html
+const countdown = document.getElementById('countdown');
+const numbersList = document.getElementById('numbers-list');
+const numberForm = document.getElementById('answers-form');
+
