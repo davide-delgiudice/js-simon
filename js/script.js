6c62df0f38b46c9f651a70f218c9d1ef61a32d07
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-27 20:33:49 +0100

    [UPD] add if condition for countdown

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ let seconds = 10;
 // assegno i secondi all'interno del countdown
 countdown.innerText = seconds;
 
+// dichiaro una costante per decretare la fine del countdown
+const interval = setInterval(function(){
+
+    // verifico a che valore sono arrivati i secondi
+    if(seconds != 0){
+        countdown.innerText = seconds;
+    }
+    else{
+        clearInterval(interval);
+    }
+
+    // devo decrementare i secondi
+    seconds--;
+}, 1000);
