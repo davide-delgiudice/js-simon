e78130674a1eb19c89c79a5a89b0946b629379a6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-28 18:14:13 +0100

    [FIX] elements from html

--- js/script.js
+++ js/script.js
@@ -1,25 +1,8 @@
-// prendo gli elementi dalla pagina html
+// prendo gli elementi del DOM che mi servono
+const form = document.getElementById('answers-form');
+const input = document.querySelectorAll('input');
 const countdown = document.getElementById('countdown');
-const numbersList = document.getElementById('numbers-list');
-const numberForm = document.getElementById('answers-form');
-
-// dichiaro una variabile per i secondi
-let seconds = 10;
-
-// assegno i secondi all'interno del countdown
-countdown.innerText = seconds;
-
-// dichiaro una costante per decretare la fine del countdown
-const interval = setInterval(function(){
-
-    // verifico a che valore sono arrivati i secondi
-    if(seconds != 0){
-        countdown.innerText = seconds;
-    }
-    else{
-        clearInterval(interval);
-    }
-
-    // devo decrementare i secondi
-    seconds--;
-}, 1000);
+const numbers = document.getElementById('numbers-list');
+const message = document.getElementById('message');
+const instructions = document.getElementById('instructions');
+const button = document.querySelector('button');
